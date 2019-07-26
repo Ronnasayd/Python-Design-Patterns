fbe9e5c1bedca5e5162db50d21d69c8787090c0a
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-07-26 17:43:26 -0300

    prototype pattern

--- fluent_builder.py
+++ fluent_builder.py
@@ -27,5 +27,8 @@ class Pizza:
 
 
 if __name__ == '__main__':
-    pizza = Pizza.PizzaBuilder().add_garlic().add_extra_cheese().build()
+    pizza = (Pizza.PizzaBuilder()
+             .add_garlic()
+             .add_extra_cheese()
+             .build())
     print(pizza)
